a6b2aa93105a5c3f6db7b70ba48eb75941d6a8d6
Maurice Filiatreault Hebert <mfiliatreaulthebert@lecentrefranco.ca>
2024-10-25 12:16:54 -0400

    update wezterm

--- .config/wezterm/.wezterm.sync-conflict-20241017-182652-ZDZBBJR.lua
+++ .config/wezterm/.wezterm.sync-conflict-20241017-182652-ZDZBBJR.lua
@@ -1,12 +1,13 @@
--- import and variable
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local mux = wezterm.mux
 
+-- Config
 local config = {}
 config.color_scheme_dirs = { "~/.config/wezterm/color_scheme" }
 config.color_scheme = "Minimalist Dark"
 config.window_decorations = "TITLE | RESIZE"
-config.default_prog = { "C:/Users/mfiliatreaultheber/AppData/Local/Programs/Git/bin/bash.exe", "-i" }
+config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-i" }
 config.font = wezterm.font "FiraCode Nerd Font"
 config.window_background_opacity = 1
 config.font_size = 12
@@ -15,23 +16,14 @@ config.use_dead_keys = false
 config.scrollback_lines = 5000
 config.hide_tab_bar_if_only_one_tab = true
 config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
-config.window_background_opacity = 0.8
-config.automatically_reload_config = true
-config.front_end = "WebGpu"
-config.max_fps = 144
-
--- Debug configuration
--- config.exit_behavior = "Hold"
--- config.exit_behavior_messaging = "Verbose"
 
 -- Add the keybinding to trigger the script using 'send-text'
-config.keys = {
-  -- Close the current activated pane without confirmation
-  {
-    key = "x",
-    mods = "CTRL|ALT|SHIFT",
-    action = act.CloseCurrentPane { confirm = false }
-  },
-}
+-- config.keys = {
+--   {
+--     key = "b",
+--     mods = "CTRL",
+--     action = '
+--   },
+-- }
 
 return config
